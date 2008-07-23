b82b202350c2e66b2033accd758ca56aabeebe99
jaraco <none@none>
2008-07-23 04:09:25 +0000

    Converted spaces to tabs

--- jaraco/site/controllers.py
+++ jaraco/site/controllers.py
@@ -2,14 +2,14 @@ from turbogears import controllers, expose, flash
 # from jaraco import model
 import pkg_resources
 try:
-    pkg_resources.require("SQLAlchemy>=0.3.10")
+	pkg_resources.require("SQLAlchemy>=0.3.10")
 except pkg_resources.DistributionNotFound:
-    import sys
-    print >> sys.stderr, """You are required to install SQLAlchemy but appear not to have done so.
+	import sys
+	print >> sys.stderr, """You are required to install SQLAlchemy but appear not to have done so.
 Please run your projects setup.py or run `easy_install SQLAlchemy`.
 
 """
-    sys.exit(1)
+	sys.exit(1)
 from turbogears import identity, redirect
 from cherrypy import request, response
 # from jaraco import json
@@ -17,42 +17,42 @@ from cherrypy import request, response
 # log = logging.getLogger("jaraco.controllers")
 
 class Root(controllers.RootController):
-    @expose(template="jaraco.site.templates.welcome")
-    # @identity.require(identity.in_group("admin"))
-    def index(self):
-        import time
-        return dict(now=time.ctime())
+	@expose(template="jaraco.site.templates.welcome")
+	# @identity.require(identity.in_group("admin"))
+	def index(self):
+		import time
+		return dict(now=time.ctime())
 
-    @expose(template="jaraco.site.templates.login")
-    def login(self, forward_url=None, previous_url=None, *args, **kw):
+	@expose(template="jaraco.site.templates.login")
+	def login(self, forward_url=None, previous_url=None, *args, **kw):
 
-        if not identity.current.anonymous \
-            and identity.was_login_attempted() \
-            and not identity.get_identity_errors():
-            raise redirect(forward_url)
+		if not identity.current.anonymous \
+			and identity.was_login_attempted() \
+			and not identity.get_identity_errors():
+			raise redirect(forward_url)
 
-        forward_url=None
-        previous_url= request.path
+		forward_url=None
+		previous_url= request.path
 
-        if identity.was_login_attempted():
-            msg=_("The credentials you supplied were not correct or "
-                   "did not grant access to this resource.")
-        elif identity.get_identity_errors():
-            msg=_("You must provide your credentials before accessing "
-                   "this resource.")
-        else:
-            msg=_("Please log in.")
-            forward_url= request.headers.get("Referer", "/")
+		if identity.was_login_attempted():
+			msg=_("The credentials you supplied were not correct or "
+				   "did not grant access to this resource.")
+		elif identity.get_identity_errors():
+			msg=_("You must provide your credentials before accessing "
+				   "this resource.")
+		else:
+			msg=_("Please log in.")
+			forward_url= request.headers.get("Referer", "/")
 
-        response.status=403
-        return dict(message=msg, previous_url=previous_url, logging_in=True,
-                    original_parameters=request.params,
-                    forward_url=forward_url)
+		response.status=403
+		return dict(message=msg, previous_url=previous_url, logging_in=True,
+					original_parameters=request.params,
+					forward_url=forward_url)
 
-    @expose()
-    def logout(self):
-        identity.current.logout()
-        raise redirect("/")
+	@expose()
+	def logout(self):
+		identity.current.logout()
+		raise redirect("/")
 
 	@expose(template="jaraco.site.templates.project")
 	def projects(self, name):
