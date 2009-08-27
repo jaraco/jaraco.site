5a6bb93c5bf319664065b649af8cb2b8a5986a81
jaraco <none@none>
2009-08-27 03:14:57 +0000

    Modified script to use Python 3 print syntax

--- isapiapp.py
+++ isapiapp.py
@@ -1,10 +1,11 @@
 #!python
+from __future__ import print_function
 
 from jaraco.site.isapi import (
 	factory as __ExtensionFactory__,
 	handle_command_line, setup_environment,
 	)
-print 'setting up environment'
+print('setting up environment')
 setup_environment(__file__)
-print 'done setting up environment'
+print('done setting up environment')
 if __name__ == '__main__': handle_command_line()
