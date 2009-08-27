4fbf182075b467ebd4dea6dc478b10a856cf4bd7
jaraco <none@none>
2009-08-27 01:20:01 +0000

    Forgot setup_environment hook

--- isapiapp.py
+++ isapiapp.py
@@ -1,5 +1,8 @@
 #!python
 
-from jaraco.site.isapi import factory as __ExtensionFactory__, handle_command_line
+from jaraco.site.isapi import (
+	factory as __ExtensionFactory__,
+	handle_command_line, setup_environment,
+	)
+setup_environment(__file__)
 if __name__ == '__main__': handle_command_line()
-
