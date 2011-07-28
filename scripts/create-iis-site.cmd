064537765ec70563642c10045c528e81286e2ae2
Jason R. Coombs <jaraco@jaraco.com>
2011-07-27 23:13:30 -0400

    create-iis-site now creates the app pool also

--- scripts/create-iis-site.cmd
+++ scripts/create-iis-site.cmd
@@ -1,3 +1,5 @@
 @echo off
 \windows\system32\inetsrv\appcmd add site /id:3 /name:"Primary Web Site" /physicalPath:c:\inetpub\jaraco.site /bindings:http/*:80:www.jaraco.com,https/*:443:www.jaraco.com
-echo "This script does not create an application pool for the site. You must manually create the application pool "Primary Web Site" and connect it to this site
+\windows\system32\inetsrv\appcmd add apppool /name:"Primary Web Site"
+\windows\system32\inetsrv\appcmd set app "Primary Web Site/" /applicationPool:"Primary Web Site"
+mkdir \inetpub\jaraco.site
