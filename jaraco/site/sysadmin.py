7815152be36ea3a9dfd2d8041cc5b2509e075a2e
Jason R. Coombs <jaraco@jaraco.com>
2013-07-20 04:55:33 -0400

    Clean up NTUser object

--- jaraco/site/sysadmin.py
+++ jaraco/site/sysadmin.py
@@ -7,17 +7,22 @@ class NTUser(object):
 		objectString = 'WinNT://%(system)s/%(userid)s,user' % vars()
 		try:
 			self.user = win32com.client.GetObject(objectString)
-		except com_error, (hr,msg,exc,arg):
+		except com_error as exc:
+			hr, msg, exc, arg = exc
 			if hr==0x8007007b:
-				raise ValueError, "Account not found or permission denied locating account."
-			raise ValueError, "Unknown error (0x%(hr)x - %(msg)s) opening %(objectString)s (does IIS user have privilege to Read All Properties?)" % vars()
+				raise ValueError("Account not found or permission denied "
+					"locating account.")
+			raise ValueError("Unknown error (0x%(hr)x - %(msg)s) opening "
+				"%(objectString)s (does IIS user have privilege to Read All "
+				"Properties?)" % vars())
 
 	def reset(self, OldPasswd, NewPasswd):
 		try:
 			self.user.ChangePassword(OldPasswd, NewPasswd)
-			# You could use the following instead if you're running under admin privileges
-			# self.adsNTUser.SetPassword(NewPasswd)
-		except com_error, (hr, msg, exc, arg):
+			# You could use the following instead if you're running under
+			# admin privileges self.adsNTUser.SetPassword(NewPasswd)
+		except com_error as exc:
+			hr, msg, exc, arg = exc
 			scode = hex(exc[5])
 			if (scode == "0x8007005"):
 				msg = "Your NT Account is locked out."
@@ -29,4 +34,4 @@ class NTUser(object):
 				msg = "Your new password does not meet the policy requirements for passwords on this system."
 			else:
 				msg = "ADSI Error - 0x%(hr)x: %(msg)s, %(scode)s" % vars()
-			raise ValueError, msg
+			raise ValueError(msg)
