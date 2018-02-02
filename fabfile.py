c308a7d12f33dae39db4813f899342932608cc8c
Jason R. Coombs <jaraco@jaraco.com>
2018-02-01 21:34:46 -0500

    Update deployment technique to use venv and Python 3.6

--- fabfile.py
+++ fabfile.py
@@ -1,14 +1,12 @@
 """
 Routines for installing, staging, and serving recapturedocs on Ubuntu.
 
-To install on a clean Ubuntu Trusty box, simply run
+To install on a clean Ubuntu Xenial box, simply run
 fab bootstrap
 """
 
 from fabric.api import sudo, run, task, env
-from fabric.context_managers import shell_env
 from fabric.contrib import files
-from jaraco.text import local_format as lf
 from more_itertools.recipes import flatten
 
 if not env.hosts:
@@ -22,6 +20,7 @@ def bootstrap():
 	install_dependencies()
 	install_env()
 	update()
+	install_service()
 	configure_nginx()
 	install_cert()
 
@@ -33,56 +32,49 @@ def install_dependencies():
 	# lets encrypt for certificates
 	sudo('apt install -y letsencrypt')
 
+	sudo('apt install -y software-properties-common')
+	sudo('add-apt-repository -y ppa:deadsnakes/ppa')
+	sudo('apt update -y')
+	sudo('apt install -y python3.6 python3.6-venv')
+
 
 @task
 def install_env():
-	sudo('rm -R {install_root} || echo -n'.format(**globals()))
-	sudo('apt -q install -y python3-lxml python3-pip')
-	install_service()
+	user = run('whoami')
+	sudo(f'rm -R {install_root} || echo -n')
+	sudo(f'mkdir -p {install_root}')
+	sudo(f'chown {user} {install_root}')
+	run(f'python3.6 -m venv {install_root}')
+	run(f'{install_root}/bin/python -m pip install -U setuptools')
 
 
 @task
-def install_service(install_root=install_root):
-	sudo(lf('mkdir -p {install_root}'))
+def install_service():
 	files.upload_template(
 		"ubuntu/jaraco.site.service", "/etc/systemd/system",
-		use_sudo=True, context=vars())
+		use_sudo=True, context=globals())
 	sudo('systemctl enable jaraco.site')
 
 
 @task
-def update(version=None):
-	install_to(install_root, version, use_sudo=True)
+def update():
+	install()
 	sudo('systemctl restart jaraco.site')
 
 
-def install_to(root, version=None, use_sudo=False):
+def install():
 	"""
 	Install jaraco.site to a PEP-370 environment at root. If version is
 	not None, install that version specifically. Otherwise, use the latest.
 	"""
-	action = sudo if use_sudo else run
-	pkg_spec = 'jaraco.site'
-	if version:
-		pkg_spec += '==' + version
-	with shell_env(PYTHONUSERBASE=root):
-		usp = run('python3 -c "import site; print(site.getusersitepackages())"')
-		action('mkdir -p {usp}'.format(**locals()))
-		# can't run with '-U' because that will cause lxml to upgrade/build
-		cmd = [
-			'python3',
-			'-m', 'pip',
-			'install',
-			'--user',
-			'-U',
-			pkg_spec,
-		]
-		action(' '.join(cmd))
+	run('git clone https://github.com/jaraco/jaraco.site || echo -n')
+	run('git -C jaraco.site pull')
+	run(f'{install_root}/bin/python -m pip install ./jaraco.site')
 
 
 @task
 def remove_all():
-	sudo('stop jaraco-site || echo -n')
+	sudo('stop jaraco.site || echo -n')
 	sudo('rm /etc/init/jaraco-site.conf || echo -n')
 	sudo('rm -Rf /opt/jaraco.com')
 
