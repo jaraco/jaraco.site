3cf9763a159f60a47055a4ae9f25563d765ff11b
Jason R. Coombs <jaraco@jaraco.com>
2021-10-25 19:52:59 -0400

    Convert fabric to fabric 2

--- fabfile.py
+++ fabfile.py
@@ -7,13 +7,13 @@ fab bootstrap
 
 import itertools
 
-from fabric.api import sudo, run, task, env
-from fabric.contrib import files
+from fabric import task
+from jaraco.fabric import files
 
 flatten = itertools.chain.from_iterable
 
-if not env.hosts:
-    env.hosts = ['spidey']
+host = 'spidey'
+hosts = [host]
 
 install_root = '/opt/jaraco.com'
 
@@ -21,86 +21,86 @@ install_root = '/opt/jaraco.com'
 python = 'python3.8'
 
 
-@task
-def bootstrap():
-    install_dependencies()
-    install_env()
-    install_service()
-    update()
-    configure_nginx()
-    install_cert()
+@task(hosts=hosts)
+def bootstrap(c):
+    install_dependencies(c)
+    install_env(c)
+    install_service(c)
+    update(c)
+    configure_nginx(c)
+    install_cert(c)
 
 
-@task
-def install_dependencies():
+@task(hosts=hosts)
+def install_dependencies(c):
     # fop required by the resume endpoint
-    sudo('apt install -y fop')
+    c.sudo('apt install -y fop')
     # certbot for certificates
-    sudo('apt-add-repository -y ppa:certbot/certbot')
-    sudo('apt update -y')
-    sudo('apt install -y python-certbot-nginx')
+    c.sudo('apt-add-repository -y ppa:certbot/certbot')
+    c.sudo('apt update -y')
+    c.sudo('apt install -y python-certbot-nginx')
 
-    sudo('apt install -y software-properties-common')
-    sudo('add-apt-repository -y ppa:deadsnakes/ppa')
-    sudo('apt update -y')
-    sudo(f'apt install -y {python} {python}-venv')
-
-
-@task
-def install_env():
-    user = run('whoami')
-    sudo(f'rm -R {install_root} || echo -n')
-    sudo(f'mkdir -p {install_root}')
-    sudo(f'chown {user} {install_root}')
-    run(f'{python} -m venv {install_root}')
-    run(f'{install_root}/bin/python -m pip install -U pip')
+    c.sudo('apt install -y software-properties-common')
+    c.sudo('add-apt-repository -y ppa:deadsnakes/ppa')
+    c.sudo('apt update -y')
+    c.sudo(f'apt install -y {python} {python}-venv')
 
 
-@task
-def install_service():
+@task(hosts=hosts)
+def install_env(c):
+    user = c.run('whoami')
+    c.sudo(f'rm -R {install_root} || echo -n')
+    c.sudo(f'mkdir -p {install_root}')
+    c.sudo(f'chown {user} {install_root}')
+    c.run(f'{python} -m venv {install_root}')
+    c.run(f'{install_root}/bin/python -m pip install -U pip')
+
+
+@task(hosts=hosts)
+def install_service(c):
     files.upload_template(
         "ubuntu/jaraco.site.service",
         "/etc/systemd/system",
         use_sudo=True,
         context=globals(),
     )
-    sudo('systemctl enable jaraco.site')
+    c.sudo('systemctl enable jaraco.site')
 
 
-@task
-def update():
-    install()
-    sudo('systemctl restart jaraco.site')
+@task(hosts=hosts)
+def update(c):
+    install(c)
+    c.sudo('systemctl restart jaraco.site')
 
 
-def install():
+def install(c):
     """
     Install jaraco.site to environment at root.
     """
-    run('git clone https://github.com/jaraco/jaraco.site || echo -n')
-    run('git -C jaraco.site pull')
-    run(f'{install_root}/bin/python -m pip install -U ./jaraco.site')
+    c.run('git clone https://github.com/jaraco/jaraco.site || echo -n')
+    c.run('git -C jaraco.site pull')
+    c.run(f'{install_root}/bin/python -m pip install -U ./jaraco.site')
 
 
-@task
-def remove_all():
-    sudo('stop jaraco.site || echo -n')
-    sudo('rm /etc/init/jaraco-site.conf || echo -n')
-    sudo('rm -Rf /opt/jaraco.com')
+@task(hosts=hosts)
+def remove_all(c):
+    c.sudo('stop jaraco.site || echo -n')
+    c.sudo('rm /etc/init/jaraco-site.conf || echo -n')
+    c.sudo('rm -Rf /opt/jaraco.com')
 
 
-@task
-def configure_nginx():
-    sudo('apt install -y nginx')
+@task(hosts=hosts)
+def configure_nginx(c):
+    c.sudo('apt install -y nginx')
     source = "ubuntu/nginx config"
     target = "/etc/nginx/sites-available/jaraco.com"
-    files.upload_template(filename=source, destination=target, use_sudo=True)
-    sudo('ln -sf ../sites-available/jaraco.com /etc/nginx/sites-enabled/')
-    sudo('service nginx restart')
+    files.upload_template(c, src=source, dest=target)
+    c.sudo('ln -sf ../sites-available/jaraco.com /etc/nginx/sites-enabled/')
+    c.sudo('service nginx restart')
 
 
-@task
-def install_cert():
+@task(hosts=hosts)
+def install_cert(c):
     cmd = [
         'certbot',
         '--agree-tos',
@@ -119,4 +119,4 @@ def install_cert():
         'www.scicomm.pro',
     )
     cmd += list(flatten(['--domain', name] for name in sites))
-    sudo(' '.join(cmd))
+    c.sudo(' '.join(cmd))
