680cdde6cb7cbeb93d6592c0528ee61107485a0e
Jason R. Coombs <jaraco@jaraco.com>
2024-03-30 18:38:43 -0400

    👹 Feed the hobgoblins (delint).

--- jaraco/site/run.py
+++ jaraco/site/run.py
@@ -5,20 +5,18 @@ import cherrypy
 
 from .controllers import Root
 
-cherrypy.config.update(
-    {
-        'server.production': True,
-        'server.socket_port': int(os.environ.get('PORT', 8080)),
-        'server.socket_host': os.environ.get('SOCKET_HOST', '::'),
-        'tools.encode.on': True,
-        'tools.encode.encoding': 'utf-8',
-        'tools.decode.on': True,
-        'tools.sessions.on': True,
-        'tools.trailing_slash.on': True,
-        'tools.staticdir.root': os.fspath(files('jaraco.site')),
-        'tools.proxy.on': True,
-    }
-)
+cherrypy.config.update({
+    'server.production': True,
+    'server.socket_port': int(os.environ.get('PORT', 8080)),
+    'server.socket_host': os.environ.get('SOCKET_HOST', '::'),
+    'tools.encode.on': True,
+    'tools.encode.encoding': 'utf-8',
+    'tools.decode.on': True,
+    'tools.sessions.on': True,
+    'tools.trailing_slash.on': True,
+    'tools.staticdir.root': os.fspath(files('jaraco.site')),
+    'tools.proxy.on': True,
+})
 
 config = {
     '/static': {
