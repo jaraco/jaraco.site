9631e39ef4decf34a316df9e0fd4df4f39201a14
Jason R. Coombs <jaraco@jaraco.com>
2024-04-02 20:03:35 -0400

    Implement dark mode for projects

--- jaraco/site/projects.py
+++ jaraco/site/projects.py
@@ -1,5 +1,17 @@
+import textwrap
+
 import cherrypy
 from jaraco.develop import git, filters
+
+
+style = textwrap.dedent("""
+    @media (prefers-color-scheme: dark) {
+      body {
+        background-color: #444;
+        color: white;
+      }
+    }
+    """)
 
 
 class Projects:
@@ -7,10 +19,10 @@ class Projects:
     def index(self):
         projects = filter(filters.Tag('not fork'), git.projects())
         doc = '<br>\n'.join(map(self.make_badge, projects))
-        return f'<html><body>\n{doc}\n</body></html>'
+        return f'<html><head><style>{style}</style></head><body>\n{doc}\n</body></html>'
 
     def make_badge(self, project):
         url = git.resolve(project + '/')
         badge = url.join('actions/workflows/main.yml/badge.svg').resolved
         workflow = url.join('actions?query=workflow%3A%22tests%22').resolved
-        return f'<div><a href="{workflow}"><img src="{badge}"> {project}</a></div>'
+        return f'<div><a href="{workflow}"><img src="{badge}"></a> {project}</div>'
