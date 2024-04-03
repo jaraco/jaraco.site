cfde34ad4ae37dce2cc2c2106815dc04929cd71f
Jason R. Coombs <jaraco@jaraco.com>
2024-04-03 06:36:16 -0400

    Excluded some items from the project list.

--- jaraco/site/projects.py
+++ jaraco/site/projects.py
@@ -1,7 +1,34 @@
 import textwrap
+from itertools import filterfalse
 
 import cherrypy
 from jaraco.develop import git, filters
+
+
+excludes = [
+    '/cherrypy/cheroot',
+    '/cherrypy/cherrypy',
+    'compilers',
+    'dotfiles',
+    'git-tools',
+    'jaraco.github.io',
+    'jaraco.pghfreethought',
+    'jaraco/tidelift',
+    'moneydance',
+    'multipy-tox',
+    'omaha-data',
+    'scicomm.pro',
+    'skeleton',
+    'twine',
+]
+
+
+def make_url(project):
+    return git.resolve(project + '/')
+
+
+def exclude(project):
+    return any(map(make_url(project).__contains__, excludes))
 
 
 style = textwrap.dedent("""
@@ -11,18 +38,26 @@ style = textwrap.dedent("""
         color: white;
       }
     }
+    .container {
+      /* column-width: 20em; */
+    }
     """)
 
 
 class Projects:
     @cherrypy.expose
     def index(self):
-        projects = filter(filters.Tag('not fork'), git.projects())
+        projects = sorted(
+            filterfalse(exclude, filter(filters.Tag('not fork'), git.projects())),
+            key=lambda s: s.replace('/', '|'),
+        )
         doc = '<br>\n'.join(map(self.make_badge, projects))
-        return f'<html><head><style>{style}</style></head><body>\n{doc}\n</body></html>'
+        return f'<html><head><style>{style}</style></head><body><div class="container">\n{doc}\n</div></body></html>'
 
     def make_badge(self, project):
-        url = git.resolve(project + '/')
+        url = make_url(project)
         badge = url.join('actions/workflows/main.yml/badge.svg').resolved
         workflow = url.join('actions?query=workflow%3A%22tests%22').resolved
-        return f'<div><a href="{workflow}"><img src="{badge}"></a> {project}</div>'
+        return (
+            f'<div><h3>{project}</h3><a href="{workflow}"><img src="{badge}"></a></div>'
+        )
---
+++ newsfragments/+facac4f9.feature.rst
@@ -0,0 +1,1 @@
+Excluded some items from the project list.
